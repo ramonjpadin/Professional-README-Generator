b21fd5233c4fb3630819437256052fe6db19a17a
Ramon Padin <ramonjpadin@gmail.com>
2022-10-19 16:09:50 -0500

    added gitignore

---
+++ index.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+var inquirer = require('inquirer');
+
+// questions //
+let questions = [
+    {
+        type: "input",
+        message: "What is the title of your repository?",
+        name: "title",
+    }, {
+        type: "input",
+        message: "What is the title of your repository?",
+        name: "title"
+    }
+]
